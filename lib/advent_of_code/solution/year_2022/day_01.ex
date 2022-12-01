a43153508dfca0f8f850f3744d155f78b009a27b
Guillermo <guillermo@radiobotics.com>
2022-12-01 07:08:55 +0100

    Aoc day 1, 2022

--- lib/advent_of_code/solution/year_2022/day_01.ex
+++ lib/advent_of_code/solution/year_2022/day_01.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Solution.Year2022.Day01 do
-  def part1(_input) do
+  def part1(input) do
+    input
+    |> parse_input()
+    # |> Enum.map(fn x -> Enum.sum(x) end)
+    # |> Enum.max()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn x -> Enum.sum(x) end)
+    |> Enum.sort(&(&1 >= &2))
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  def parse_input(args) do
+    args
+    |> String.split("\n")
+    |> Enum.chunk_by(fn x -> x == "" end)
+    |> Enum.filter(fn [a | b] -> a != "" end)
+    |> Enum.map(fn x -> Enum.map(x, &String.to_integer/1) end)
   end
 end
