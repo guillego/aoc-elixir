ae049888b380a1c0aca9525775e28dd999366af1
Guillermo <guillermo@radiobotics.com>
2022-12-02 07:58:44 +0100

    AoC day 2, 2022

--- lib/advent_of_code/solution/year_2022/day_02.ex
+++ lib/advent_of_code/solution/year_2022/day_02.ex
@@ -1,7 +1,86 @@
 defmodule AdventOfCode.Solution.Year2022.Day02 do
-  def part1(_input) do
+  @translation %{
+    "X" => :rock,
+    "Y" => :paper,
+    "Z" => :scissors,
+    "A" => :rock,
+    "B" => :paper,
+    "C" => :scissors
+  }
+  @translation2 %{
+    "X" => :lose,
+    "Y" => :draw,
+    "Z" => :win,
+    "A" => :rock,
+    "B" => :paper,
+    "C" => :scissors
+  }
+  @points %{rock: 1, paper: 2, scissors: 3}
+  @scores %{win: 6, draw: 3, lose: 0}
+
+  def part1(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn [a, b] -> [@translation[a], @translation[b]] end)
+    |> Enum.map(&play_rps(&1))
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def play_rps([a, b] = _inputs) when a == b do
+    @points[b] + @scores[:draw]
+  end
+
+  def play_rps([_a, b] = inputs) do
+    outcome =
+      case inputs do
+        [:rock, :paper] -> :win
+        [:rock, :scissors] -> :lose
+        [:paper, :scissors] -> :win
+        [:paper, :rock] -> :lose
+        [:scissors, :rock] -> :win
+        [:scissors, :paper] -> :lose
+      end
+
+    @points[b] + @scores[outcome]
+  end
+
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn [a, b] -> [@translation2[a], @translation2[b]] end)
+    |> Enum.map(&play_rps_rigged(&1))
+    |> Enum.sum()
+  end
+
+  def play_rps_rigged([a, b] = _inputs) when b == :win do
+    choose =
+      case a do
+        :rock -> :paper
+        :paper -> :scissors
+        :scissors -> :rock
+      end
+
+    @points[choose] + @scores[b]
+  end
+
+  def play_rps_rigged([a, b] = _inputs) when b == :draw do
+    @points[a] + @scores[b]
+  end
+
+  def play_rps_rigged([a, b] = _inputs) when b == :lose do
+    choose =
+      case a do
+        :rock -> :scissors
+        :paper -> :rock
+        :scissors -> :paper
+      end
+
+    @points[choose] + @scores[b]
+  end
+
+  def parse_input(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1))
   end
 end
