a68b82766de42bb85787606aab684c1f9f6b082f
Guillermo <guillermo@radiobotics.com>
2022-12-10 22:12:24 +0100

    Aoc day 4, 2021 - WIP

--- lib/advent_of_code/solution/year_2021/day_04.ex
+++ lib/advent_of_code/solution/year_2021/day_04.ex
@@ -1,7 +1,31 @@
 defmodule AdventOfCode.Solution.Year2021.Day04 do
-  def part1(_input) do
+  def part1(input) do
+    {numbers, board} = parse_input(input)
+    board
   end
 
   def part2(_input) do
+  end
+
+
+  defp parse_input(input) do
+    [numbers_str | boards_str] = String.split(input, "\n\n", trim: true)
+
+    numbers = split_to_int(numbers_str, ",", trim: true)
+    board = Enum.map(boards_str, &parse_board(&1))
+
+    {numbers, board}
+  end
+
+  defp parse_board(board_str) do
+    board_str
+    |> String.split("\n", trim: true)
+    |> Enum.map(&split_to_int(&1, " ", trim: true))
+  end
+
+  defp split_to_int(input, pattern, split_options) do
+    input
+    |> String.split(pattern, split_options)
+    |> Enum.map(&String.to_integer(&1))
   end
 end
